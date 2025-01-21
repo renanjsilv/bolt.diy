ceecb0994fec84627dfd323450a00f2ff4fc36c2
Renan Silva <82475449+renanjsilv@users.noreply.github.com>
2025-01-21 09:09:15 -0300

    Update ExamplePrompts.tsx

--- app/components/chat/ExamplePrompts.tsx
+++ app/components/chat/ExamplePrompts.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const EXAMPLE_PROMPTS = [
-  { text: 'Build a todo app in React using Tailwind' },
-  { text: 'Build a simple blog using Astro' },
-  { text: 'Create a cookie consent form using Material UI' },
-  { text: 'Make a space invaders game' },
-  { text: 'Make a Tic Tac Toe game in html, css and js only' },
+  { text: 'Crie um aplicativo to-do usando React e Tailwind' },
+  { text: 'Crie um blog simples usando Javascritp e HTML' },
+  { text: 'Crie uma aplicação de pesquisa online' },
+  { text: 'Faça o jogo do space invaders' },
+  { text: 'Faça uma loja de vendas online' },
 ];
 
 export function ExamplePrompts(sendMessage?: { (event: React.UIEvent, messageInput?: string): void | undefined }) {
